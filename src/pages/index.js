8b57abdaa53c8c4865064ca2ba0d731d3e6307db
Gaál Norbert <gaalnorbert@Gaal-MacBook-Pro.local>
2018-09-07 17:31:57 +0200

    my first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi guys</h1>
+    <p>Fuck, I started to code. Code as a designer!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
