25af03a42c2a518e0fe89252f1682e2ae585ceaa
Gaál Norbert <gaalnorbert@Gaal-MacBook-Pro.local>
2018-09-12 17:14:50 +0200

    button hover animation and header animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,20 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
    <div className="Hero">
+    <div>
+    <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div>
     <div className="HeroGroup">
       <h1>Design Hub</h1>
       <p>Digitális terméktervezés A-Z-ig. Kutatás, design, kód</p>
