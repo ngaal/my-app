fc0a47cca39365e758899eb3dde622ea6cacf1bc
Gaál Norbert <gaalnorbert@Gaal-MacBook-Pro.local>
2018-09-13 22:12:55 +0200

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,40 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/card';
 
 const IndexPage = () => (
-  <div>
-   <div className="Hero">
+
+<div>
+  <div className="Hero">
     <div>
-    <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
-    <div className="HeroGroup">
-      <h1>Design Hub</h1>
-      <p>Digitális terméktervezés A-Z-ig. Kutatás, design, kód</p>
-      <Link to="/page-2/">Megnézem az introt</Link>
-    </div>    
-   </div>
-  </div>
+      <div className="HeroGroup">
+        <h1>Design Hub</h1>
+        <p>Digitális terméktervezés A-Z-ig. Kutatás, design, kód</p>
+        <Link to="/page-2/">Megnézem az introt</Link>
+      </div>    
+    </div>
+  <div className="Cards">
+    <h2> Legújabb kurzusok </h2>
+      <Card 
+        title="Sketch alapjai"
+        text="12 Rész, 3 óra videó"
+        text="Formák és rétegek, vektorok, symbols, nested symbols, könyvtárak."
+        text="Free"/>
+    </div>
+</div>
 )
 
 export default IndexPage
