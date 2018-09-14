1945544c24626862af7d45d20457cdb98f03889d
Gaál Norbert <gaalnorbert@Gaal-MacBook-Pro.local>
2018-09-14 18:23:28 +0200

    Cards, Cardgroup, props and more things

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './card.css'
 
-const card = props => (
+const Card = props => (
     <div className="Card">
         <h3>{props.title}</h3>
-        <p className="caption">{props.text}</p>
-        <p>{props.text}</p>
-        <p className="Price">{props.text}</p>
+        <p className="Caption">{props.caption}</p>
+        <p className="Description">{props.description}</p>
+        <p className="Price"><span>{props.price}</span></p>
         <img src={props.image}></img>
     </div>
 )
 
-export default card
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,12 +28,21 @@ const IndexPage = () => (
     </div>
   <div className="Cards">
     <h2> Legújabb kurzusok </h2>
+    <div className="CardGroup">
       <Card 
-        title="Sketch alapjai"
-        text="12 Rész, 3 óra videó"
-        text="Formák és rétegek, vektorok, symbols, nested symbols, könyvtárak."
-        text="Free"/>
+        title="Sketch alapjai I."
+        caption="12 Rész, 3 óra videó"
+        description="Formák és rétegek, vektorok, symbols, nested symbols, könyvtárak."
+        price="Free"
+        image={require('../images/card.png')} />
+      <Card 
+        title="Product Design I."
+        caption="9 Rész, 5 óra videó"
+        description="IOS design guideline, tipográfia, színek, ikonok, komponensek, tervezés különböző eszközökre"
+        price="18.990Ft"
+        image={require('../images/card-1.png')} />
     </div>
+  </div>
 </div>
 )
 
